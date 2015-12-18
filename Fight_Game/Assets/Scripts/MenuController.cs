092df158766a589a1d39ad7becb2edaaac0be6dc
kf953741 <K@KangMacBook-Pro.local>
2015-12-18 19:13:10 +0800

    完成换装系统,和按键监听框架

---
+++ Fight_Game/Assets/Scripts/MenuController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+	public Color purple;
+	public SkinnedMeshRenderer headMesh;
+	public SkinnedMeshRenderer handMesh;
+	public Mesh[] headMeshs;
+	private int headMeshIndex=0;
+	public Mesh[] handMeshs;
+	private int handMeshIndex=0;
+
+	private Button btnHeadChange;
+	private GameObject btnHandChange;
+	void Awake()
+	{
+
+	}
+
+	void Start()
+	{
+		btnHeadChange = GameObject.Find(@"head/btn_headChange").GetComponent<Button>();
+		btnHandChange = GameObject.Find(@"hand/btn_handChange").gameObject;
+		UIEventListener.Get(btnHandChange).onClick = OnHandMeshChange; 
+		btnHeadChange.onClick.AddListener(OnHeadMeshChange);
+	}
+	public  void OnHeadMeshChange()
+	{
+		headMeshIndex++;
+		headMeshIndex%=headMeshs.Length;
+		headMesh.sharedMesh = headMeshs[headMeshIndex];
+	}
+	public void OnHandMeshChange(GameObject go)
+	{
+		handMeshIndex++;
+		handMeshIndex%=handMeshs.Length;
+		handMesh.sharedMesh = handMeshs[handMeshIndex];
+	}
+	public void OnChangeBlue()
+	{
+		OnChangeColor(Color.blue);
+	}
+	public void OnChangeGreen()
+	{
+		OnChangeColor(Color.green);
+	}
+	public void OnChangePurple()
+	{
+		OnChangeColor(purple);
+	}
+	public void OnChangeRed()
+	{
+		OnChangeColor(Color.red);
+	}
+	public void OnChangeLightGreen()
+	{
+		OnChangeColor(Color.cyan);
+	}
+	private void OnChangeColor(Color c)
+	{
+
+	}
+	public void OnClickPlay()
+	{
+
+	}
+}
---
+++ Fight_Game/Assets/Scripts/UIEventListener.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class UIEventListener : MonoBehaviour ,IPointerClickHandler
+{
+	public delegate void VoidDelegate(GameObject go);
+	public VoidDelegate onClick;
+	public static UIEventListener Get(GameObject go)
+	{
+		UIEventListener listener = go.GetComponent<UIEventListener>();
+		if(listener==null)
+			listener = go.AddComponent<UIEventListener>();
+		return listener;
+	}
+	public void OnPointerClick (PointerEventData eventData)
+	{
+		if(onClick!=null)
+			onClick(gameObject);
+	}
+}
